047fbe83b9ee825e6469b8e5e27161f8034bcec6
Max Malevich <malevich.m.439-1@e.tusur.ru>
2022-02-26 01:06:14 +0700

    fixed bags

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -6,7 +6,6 @@ import ErrorMessage from '../errorMessage/ErrorMessage';
 
 import './charList.scss';
 
-
 class CharList extends Component {
     state = {
         chars: [],
@@ -57,10 +56,29 @@ class CharList extends Component {
             .catch(this.onError);
     }
 
+    itemRefs = [];
+
+    setRef = (ref) => {
+        this.itemRefs.push(ref);
+    }
+
+    focusOnItem = (id) => {
+        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemRefs[id].classList.add('char__item_selected');
+        this.itemRefs[id].focus();
+    }
+
+    enterOnItem = (id) => {
+        this.itemRefs[id].classList.add('char__item_focused');
+    }
+
+    leaveFromItem = () => {
+        this.itemRefs.forEach(item => item.classList.remove('char__item_focused'));
+    }
 
     renderItem = () => {
         const chars = this.state.chars;
-        const items =  chars.map((item) => {
+        const items =  chars.map((item, i) => {
             let imgStyle = {'objectFit' : 'cover'};
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
                 imgStyle = {'objectFit' : 'unset'};
@@ -70,7 +88,13 @@ class CharList extends Component {
                 <li 
                     className="char__item"
                     key={item.id}
-                    onClick={() => this.props.onCharSelected(item.id)}>
+                    ref={this.setRef}
+                    onClick={() => {
+                        this.props.onCharSelected(item.id);
+                        this.focusOnItem(i);
+                    }}
+                    onMouseEnter={() => this.enterOnItem(i)}
+                    onMouseLeave={this.leaveFromItem}>
                         <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                         <div className="char__name">{item.name}</div>
                 </li>
@@ -95,11 +119,9 @@ class CharList extends Component {
 
         return (
             <div className="char__list">
-                <ul className="char__grid">
                     {errorMessage}
                     {spinner}
                     {content}
-                </ul>
                 <button 
                 onClick = {() => this.onRequest(offset)}
                 disabled={newItemLoading}
